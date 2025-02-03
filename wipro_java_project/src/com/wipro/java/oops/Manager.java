99a6b60c2177822b32cbcfbcb13c7fa7ad82c919
SirishaVemparala-Wipro <sirishavemparala1203@gmail.com>
2025-02-03 12:29:14 +0530

    Day 2 Java

--- wipro_java_project/src/com/wipro/java/oops/Manager.java
+++ wipro_java_project/src/com/wipro/java/oops/Manager.java
@@ -16,5 +16,17 @@ public class Manager extends Employee {
 	public Manager() {
 		// TODO Auto-generated constructor stub
 	}
+	public static void main (String a[]) {
+		Employee employee=new Manager();//create a Manager object and set its properties inherited from Employee
+		employee.setId(5678);//Setting Employee's ID
+		employee.setName("mark");//setting employee's name
+		employee.setDepartment("Testing");//setting employee's department
+		employee.setSalary(40000f);//setting employee's salary
+		
+		System.out.println(employee.getId());//retrieving Employee's ID
+		System.out.println(employee.getName());//retrieving employee's name
+		System.out.println(employee.getDepartment());//retrieving employee's department
+		System.out.println(employee.getSalary());//retrieving employee's salary
+	}
 
 }
--- wipro_java_project/src/com/wipro/java/oops/Projectlead.java
+++ wipro_java_project/src/com/wipro/java/oops/Projectlead.java
@@ -14,7 +14,27 @@ public class Projectlead extends Employee{
 	 * 
 	 */
 	public Projectlead() {
+		
+	}
 		// TODO Auto-generated constructor stub
+		public static void main(String a[]) {
+			//child object is instantiated from the child constructor
+			//parent class consumes the props and behaviors of child class
+			/*Projectlead projectlead=new Projectlead();
+			Projectlead projectLead1=(Projectlead) new Employee();*/
+			Employee employee=new Projectlead();//create a Projectlead object and set its properties inherited from Employee
+			employee.setId(1234);//Setting Employee's ID
+			employee.setName("andrew");//setting employee's name
+			employee.setDepartment("Marketing");//setting employee's department
+			employee.setSalary(35000f);//setting employee's salary
+			
+			System.out.println(employee.getId());//retrieving Employee's ID
+			System.out.println(employee.getName());//retrieving employee's name
+			System.out.println(employee.getDepartment());//retrieving employee's department
+			System.out.println(employee.getSalary());//retrieving employee's salary
+			
+		
 	}
 
 }
+
